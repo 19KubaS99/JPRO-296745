36daea227e272d1c5c8fe6a80887055f579f035b
19KubaS99 <48121484+19KubaS99@users.noreply.github.com>
2019-06-11 21:18:40 +0200

    Poszerzono obsługę config.txt, dodano wypisywanie danych do pliku wyjściowego

--- main.c
+++ main.c
@@ -1,11 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-int check_config()
+void clear_data()
 {
-    int i,j;
+   FILE *data;
+
+   if ((data=fopen("data.txt", "w"))==NULL) {
+     printf ("Nie mogê otworzyæ pliku test.txt do zapisu!\n");
+     exit(1);
+     }
+}
+
+void data(int alive, int dead)
+{
+    FILE *data;
+
+   if ((data=fopen("data.txt", "a+"))==NULL) {
+     printf ("Nie mogê otworzyæ pliku test.txt do zapisu!\n");
+     exit(1);
+     }
+   fprintf (data, "alive:%d     dead:%d\n", alive, dead);
+   fclose (data);
+
+}
+
+int alive_number(char board[31][61])
+{
+    int i,j,alive=0;
+    for(i=1;i<30;i++)
+    {
+        for(j=1;j<60;j++)
+        {
+            if(board[i][j]=='X')
+                alive++;
+        }
+    }
+    return alive;
+}
+
+
+float check_seconds()
+{
+    float seconds;
+    FILE *fp;
+   if ((fp=fopen("config.txt", "r"))==NULL) {
+     printf ("Nie moge otworzyc pliku config.txt!\n");
+     exit(1);
+     }
+    fseek(fp, 75, 0);
+    fscanf(fp,"%f", &seconds);
+    fclose (fp);
+    return seconds;
+}
+
+int check_steps()
+{
+    int steps;
+    FILE *fp;
+   if ((fp=fopen("config.txt", "r"))==NULL) {
+     printf ("Nie moge otworzyc pliku config.txt!\n");
+     exit(1);
+     }
+    fseek(fp, 60, 0);
+    fscanf(fp,"%d", &steps);
+    fclose (fp);
+    return steps;
+}
+
+int check_compaction()
+{
+    int compaction;
+    FILE *fp;
+   if ((fp=fopen("config.txt", "r"))==NULL) {
+     printf ("Nie moge otworzyc pliku config.txt!\n");
+     exit(1);
+     }
+    fseek(fp, 41, 0);
+    fscanf(fp,"%d", &compaction);
+    fclose (fp);
+    return compaction;
+}
+
+int check_random()
+{
+    int i;
     FILE *fp;
    if ((fp=fopen("config.txt", "r"))==NULL) {
      printf ("Nie moge otworzyc pliku config.txt do zapisu!\n");
@@ -61,17 +140,6 @@ for(i=1;i<30;i++)
 
 }
 fclose (fp);
-/*for(i=1;i<30;i++)
-{
-    for(j=1;j<60;j++)
-    {
-        printf("%c", board[i][j]);
-    }
-    printf("\n");
-}
-
-
-   return 0;*/
 }
 void out()
 {
@@ -81,9 +149,9 @@ void out()
 void random_cells(char board[31][61])
 {
 int p,z,x,y;
+p = check_compaction() * 100;
+printf("%d", p);
 srand( time( NULL ) );
-printf("Podaj ilosc zywych komorek (max 225)\n");
-scanf("%d", &p);
 for(z=0;z<p;z++)
 {
     x = rand()%29+1;
@@ -149,25 +217,14 @@ void next_step(char board[31][61], int board2[31][61])
         }
         printf("\n");
     }
-    /*int alive=0,dead=0;
-
-    for(i=1;i<30;i++)
-    {
-        for(j=1;j<30;j++)
-        {
-            if(board[i][j] == 'X')
-                alive=alive+1;
-            else
-                dead=dead+1;
-        }
-        system("cls");
-    }*/
-
 }
 
 int main()
 {
-    int i,j,c,d,seconds=1;
+    int i,j,c,d,steps,alive,dead;
+    float seconds;
+    seconds = check_seconds();
+    steps = check_steps();
     char board[31][61];
     int board2[31][61];
     for(i=0;i<31;i++)
@@ -177,16 +234,9 @@ int main()
             board[i][j] = '.';
         }
     }
-    /*for(c = 0; c<16;c++){
-        for(d=0;d<16;d++){
-            printf("%c ", board[c][d]);
-        }
-        printf("\n");
-    }*/
-
-if (check_config()==1)
+if (check_random()==1)
     random_cells(board);
-else if(check_config()==0)
+else if(check_random()==0)
     config(board);
 else
     printf("Blad w przelaczniku random\n");
@@ -206,19 +256,19 @@ for(i=0;i<31;i++)
 }
 printf("Wcisnij dowolny klawisz by rozpoczac animacje\n");
 system("pause");
-while(i==1)
+clear_data();
+for(int s=0;s<steps;s++)
 {
-    delay(0.1);
+    delay(seconds);
     next_step(board,board2);
+   alive = alive_number(board);
+   dead = 1800 - alive;
+   int temp_alive = alive;
+   int temp_dead = dead;
+   data(alive,dead);
 }
 }
 
-    /*for(c = 0; c<16;c++){
-        for(d=0;d<16;d++){
-            printf("%d ", board2[c][d]);
-        }
-        printf("\n");
-    }*/
 
 
 
@@ -235,16 +285,4 @@ while(i==1)
 
 
 
-
- /*           count = count + (board[i-1][j-1] ? 'X' : '.');
-            count = count + (board[i-1][j] ? 'X' : '.');
-            count = count + (board[i-1][j+1] ? 'X' : '.');
-            count = count + (board[i][j-1] ? 'X' : '.');
-            count = count + (board[i][j+1] ? 'X' : '.');
-            count = count + (board[i+1][j-1] ? 'X' : '.');
-            count = count + (board[i+1][j] ? 'X' : '.');
-            count = count + (board[i+1][j+1] ? 'X' : '.');
-            board2[i][j] = count;
-            count = 0;
-*/
 
